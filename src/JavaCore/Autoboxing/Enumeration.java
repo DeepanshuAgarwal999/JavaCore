4b088d61314d0fd48811c578631aa97d14a85906
deepanshu agarwal <95143999+DeepanshuAgarwal999@users.noreply.github.com>
2023-08-28 17:27:46 +0000

    enum

--- src/JavaCore/Autoboxing/Enumeration.java
+++ src/JavaCore/Autoboxing/Enumeration.java
@@ -1,8 +1,10 @@
 package JavaCore.Autoboxing;
 
-public class Enumeration {
-//    Apple app;
-//    app = Apple.RedDel;
-//    for(Apple a : Apple.values())
-//            System.out.println(a);
+// enum{
+//     redDel
+// }
+public enum Enumeration {
+    MALE,FEMALE;
+
 }
+
---
+++ src/JavaCore/Student.java
@@ -0,0 +1,22 @@
+package JavaCore;
+
+import java.util.Enumeration;
+
+public class Student{
+    private String name;
+    private Enumeration gender;
+   
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Enumeration getGender() {
+        return gender;
+    }
+    public void setGender(Enumeration gender) {
+        this.gender = gender;
+    }        
+    }
